d805662af2a1f87518167de7d21df888cb6c2a68
Tom Drake <develtomas@gmail.com>
2017-04-13 00:29:54 +0400

    AJAX lesson - addition calculator

---
+++ ajaxLesson/AJ.js
@@ -0,0 +1,33 @@
+﻿window.addEventListener('load', action);
+
+function action() {
+    var btn = document.getElementById('btn');
+    btn.addEventListener('click', count);
+
+    function loadGo() {
+        document.getElementById('loader').style.display = 'block';
+    }
+
+    function loadEnd() {
+        document.getElementById('loader').style.display = 'none';
+    }
+
+    function count() {
+        var a = document.getElementsByName('first')[0].value,
+            b = document.getElementsByName('second')[0].value,
+            xhr = new XMLHttpRequest();
+
+        xhr.open('POST', 'CalcHandler.ashx');
+        xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+
+        xhr.onreadystatechange = function () {
+            if (xhr.readyState == 4 && xhr.status == 200) {
+                document.getElementById("res").innerHTML = xhr.responseText;
+                loadEnd();
+            }
+        }
+
+        xhr.send('a=' + a + '&b=' + b);
+        loadGo();
+    }
+}
---
+++ ajaxLesson/CalcHandler.ashx
@@ -0,0 +1,32 @@
+﻿<%@ WebHandler Language="C#" Class="CalcHandler" %>
+
+using System;
+using System.Web;
+
+// Данный обработчик используется в домашнем задании.
+
+public class CalcHandler : IHttpHandler {
+    
+    public void ProcessRequest (HttpContext context) {
+        context.Response.ContentType = "text/plain";
+        System.Threading.Thread.Sleep(1500);
+        try
+        {                
+            int a = Convert.ToInt32(context.Request.Form["a"]);
+            int b = Convert.ToInt32(context.Request.Form["b"]);
+
+            context.Response.Write(a + b);
+        }
+        catch
+        {
+            context.Response.Write("Ошибка, handler не обработал запрос.");
+        }
+    }
+ 
+    public bool IsReusable {
+        get {
+            return false;
+        }
+    }
+
+}
